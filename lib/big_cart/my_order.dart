7484c5fd3dff46254871579a0d2c407249f84448
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-07-13 18:48:48 +0530

    my order

--- lib/big_cart/my_order.dart
+++ lib/big_cart/my_order.dart
@@ -31,6 +31,86 @@ class _MyOrderState extends State<MyOrder> {
                   ),
                 ],
               ),
+              const SizedBox(height: 20),
+              Container(
+                width: 380,
+                height: 294,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/images/order.png",
+                                height: 80,
+                                width: 80,
+                              ),
+                              const SizedBox(width: 15),
+                            ],
+                          ),
+                          const SizedBox(width: 15),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Order #90897 ",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Poppins",
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Placed on October 19 2021 ",
+                                style: TextStyle(
+                                  color: Color(0xFF868889),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Items: 10 Items: \$16.90",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 50, top: 20),
+                            child: Image.asset(
+                              "assets/images/my card.png",
+                              height: 25,
+                              width: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 12),
+                      Divider(
+                        height: 2,
+                        color: Color(0xFFEBEBEB),
+                        thickness: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
