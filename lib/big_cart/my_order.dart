9b1e047a4ae093b686d4f8296dc9968e3ec45e09
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-07-17 11:43:19 +0530

    stepper screen

--- lib/big_cart/my_order.dart
+++ lib/big_cart/my_order.dart
@@ -75,8 +75,8 @@ class _MyOrderState extends State<MyOrder> {
               ),
               const SizedBox(height: 20),
               Container(
-                width: 380,
-                height: 500,
+                width: double.infinity,
+                height: 294,
                 decoration: const BoxDecoration(
                   color: Color(0xFFFFFFFF),
                 ),
@@ -149,60 +149,358 @@ class _MyOrderState extends State<MyOrder> {
                         color: Color(0xFFEBEBEB),
                         thickness: 2,
                       ),
-                      Theme(
-                        data: ThemeData(
-                          canvasColor: Colors.yellow,
-                          colorScheme: Theme.of(context).colorScheme.copyWith(
-                                primary: Colors.green,
-                                background: Colors.red,
-                                secondary: Colors.green,
+                      // Theme(
+                      //   data: ThemeData(
+                      //     canvasColor: Colors.yellow,
+                      //     colorScheme: Theme.of(context).colorScheme.copyWith(
+                      //           primary: Colors.green,
+                      //           background: Colors.red,
+                      //           secondary: Colors.green,
+                      //         ),
+                      //   ),
+                      //   child: Stepper(
+                      //     currentStep: currentStep,
+                      //     onStepContinue: continueStep,
+                      //     onStepCancel: cancelStep,
+                      //     onStepTapped: onStepTapped,
+                      //     controlsBuilder: controlsBuilder,
+                      //     steps: [
+                      //       Step(
+                      //         title: const Text(
+                      //           "Order placed",
+                      //         ),
+                      //         content: const Text(
+                      //           "",
+                      //           // "This is second step",
+                      //         ),
+                      //         isActive: currentStep >= 0,
+                      //       ),
+                      //       Step(
+                      //         title: const Text(
+                      //           "Order confirmed",
+                      //         ),
+                      //         content: const Text(
+                      //           "This is third step",
+                      //         ),
+                      //         isActive: currentStep >= 1,
+                      //       ),
+                      //       Step(
+                      //         title: const Text(
+                      //           "Order shipped",
+                      //         ),
+                      //         content: const Text(
+                      //           "This is forth step",
+                      //         ),
+                      //         isActive: currentStep >= 2,
+                      //       ),
+                      //       const Step(
+                      //         title: Text(
+                      //           "Order placed",
+                      //         ),
+                      //         content: Text(
+                      //           "This is fifth step",
+                      //         ),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                height: 110,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/images/order.png",
+                                height: 80,
+                                width: 80,
                               ),
-                        ),
-                        child: Stepper(
-                          currentStep: currentStep,
-                          onStepContinue: continueStep,
-                          onStepCancel: cancelStep,
-                          onStepTapped: onStepTapped,
-                          controlsBuilder: controlsBuilder,
-                          steps: [
-                            Step(
-                              title: const Text(
-                                "Order placed",
+                              const SizedBox(width: 15),
+                            ],
+                          ),
+                          const SizedBox(width: 15),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Order #90897 ",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Poppins",
+                                  fontSize: 15,
+                                ),
                               ),
-                              content: const Text(
-                                "",
-                                // "This is second step",
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Placed on October 19 2021 ",
+                                style: TextStyle(
+                                  color: Color(0xFF868889),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 13,
+                                ),
                               ),
-                              isActive: currentStep >= 0,
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Items: 10 Items: \$16.90",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 50, top: 20),
+                            child: Image.asset(
+                              "assets/images/my card.png",
+                              height: 25,
+                              width: 25,
                             ),
-                            Step(
-                              title: const Text(
-                                "Order confirmed",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                height: 148,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/images/order.png",
+                                height: 80,
+                                width: 80,
                               ),
-                              content: const Text(
-                                "This is third step",
+                              const SizedBox(width: 15),
+                            ],
+                          ),
+                          const SizedBox(width: 15),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Order #90897 ",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Poppins",
+                                  fontSize: 15,
+                                ),
                               ),
-                              isActive: currentStep >= 1,
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Placed on October 19 2021 ",
+                                style: TextStyle(
+                                  color: Color(0xFF868889),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Items: 10 Items: \$16.90",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 50, top: 20),
+                            child: Image.asset(
+                              "assets/images/my card.png",
+                              height: 25,
+                              width: 25,
                             ),
-                            Step(
-                              title: const Text(
-                                "Order shipped",
-                              ),
-                              content: const Text(
-                                "This is forth step",
-                              ),
-                              isActive: currentStep >= 2,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(
+                        height: 2,
+                        color: Color(0xFFEBEBEB),
+                        thickness: 2,
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Container(
+                            height: 15,
+                            width: 15,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFEBEBEB),
+                              borderRadius: BorderRadius.circular(100),
                             ),
-                            const Step(
-                              title: Text(
-                                "Order placed",
-                              ),
-                              content: Text(
-                                "This is fifth step",
-                              ),
+                          ),
+                          const SizedBox(width: 30),
+                          const Text(
+                            "Order Delivered",
+                            style: TextStyle(
+                              color: Color(0xFF868889),
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w500,
                             ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(width: 110),
+                          const Text(
+                            "Aug 29 2021",
+                            style: TextStyle(
+                              color: Color(0xFF868889),
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                height: 148,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Image.asset(
+                                "assets/images/order.png",
+                                height: 80,
+                                width: 80,
+                              ),
+                              const SizedBox(width: 15),
+                            ],
+                          ),
+                          const SizedBox(width: 15),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Order #90897 ",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Poppins",
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Placed on October 19 2021 ",
+                                style: TextStyle(
+                                  color: Color(0xFF868889),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 13,
+                                ),
+                              ),
+                              Text(
+                                textAlign: TextAlign.start,
+                                "Items: 10 Items: \$16.90",
+                                style: TextStyle(
+                                  color: Color(0xFF000000),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "Poppins",
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 50, top: 20),
+                            child: Image.asset(
+                              "assets/images/my card.png",
+                              height: 25,
+                              width: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(
+                        height: 2,
+                        color: Color(0xFFEBEBEB),
+                        thickness: 2,
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Container(
+                            height: 15,
+                            width: 15,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFEBEBEB),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                          const SizedBox(width: 30),
+                          const Text(
+                            "Order Delivered",
+                            style: TextStyle(
+                              color: Color(0xFF868889),
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(width: 110),
+                          const Text(
+                            "Aug 29 2021",
+                            style: TextStyle(
+                              color: Color(0xFF868889),
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
