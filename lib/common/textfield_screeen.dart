a1dbfb398491db3854dd94733f5f02590198a616
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-07-28 19:21:50 +0530

    cliprect

--- lib/common/textfield_screeen.dart
+++ lib/common/textfield_screeen.dart
@@ -49,6 +49,120 @@ class TextFieldScreen extends StatelessWidget {
                     fontFamily: "Poppins",
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (!RegExp(r"^[a-z0-9]").hasMatch(value!)) {
+                          return "Please enter  fullName";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        isDense: true,
+                        border: InputBorder.none,
+                        hintText: "full Name",
+                        filled: true,
+                        fillColor: Color(0xFFF5F5F5),
+                        hintStyle: TextStyle(color: Color(0xFF9E9E9E), fontSize: 16, fontWeight: FontWeight.w400, fontFamily: "sf-pro-display-cufonfonts"),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (!RegExp(r"^(?:[+0]9)?[0-9]{10,12}$").hasMatch(value!)) {
+                          return "Please enter valid number";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        isDense: true,
+                        border: InputBorder.none,
+                        hintText: "phone",
+                        filled: true,
+                        fillColor: Color(0xFFF5F5F5),
+                        hintStyle: TextStyle(color: Color(0xFF9E9E9E), fontSize: 16, fontWeight: FontWeight.w400, fontFamily: "sf-pro-display-cufonfonts"),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
+                          return "Please enter valid email";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        isDense: true,
+                        border: InputBorder.none,
+                        hintText: "Email",
+                        filled: true,
+                        fillColor: Color(0xFFF5F5F5),
+                        hintStyle: TextStyle(
+                          color: Color(0xFF9E9E9E),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (!RegExp(r"(?:19|20)\d\d").hasMatch(value!)) {
+                          return "Please enter valid birthdate";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(20),
+                        isDense: true,
+                        border: InputBorder.none,
+                        hintText: "date of birth",
+                        filled: true,
+                        fillColor: Color(0xFFF5F5F5),
+                        hintStyle: TextStyle(
+                          color: Color(0xFF9E9E9E),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
